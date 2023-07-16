c46c439a7800c1d17dd5285f5d5e8e26d6879504
Sanjith_R_K <130780080+Sanjith-R-K@users.noreply.github.com>
2023-07-16 12:00:57 +0530

    Update package.jsx

--- frontend/src/Components/pages/packages/package.jsx
+++ frontend/src/Components/pages/packages/package.jsx
@@ -4,67 +4,17 @@ import Navbar from '../../Navbar1/Navbar';
 import Footer from '../../Footer/Footer';
 import '../../Popular/popular.css';
 import '../../../app.css';
-// import { BsArrowRightShort } from 'react-icons/bs';
-// import { MdLocationOn } from 'react-icons/md';
-// import { BsPersonFill } from 'react-icons/bs';
-// import { GiDuration } from 'react-icons/gi';
-// import { MdModeOfTravel } from 'react-icons/md';
-// import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { Link } from 'react-router-dom';
-// import img from '../../../Assests/Baga/b1.jpg';
 
 const Packages = () => {
 
 
   const result = () => {
-    window.search(); // Call the function defined in index.html
+    window.search(); 
   };
 
 
-
-  // const Offer = [
-  //   {
-  //     per: 'Ulimated',
-  //     off: 28,
-  //     duration: 5,
-  //     mode: 'Train',
-  //     imgSrc: img,
-  //     location: 'Ooty',
-  //     price: '8219',
-  //     tag: '/',
-  //   },
-  //   {
-  //     per: 'Ulimated',
-  //     off: 15,
-  //     duration: 3,
-  //     mode: 'Flight',
-  //     imgSrc: img,
-  //     location: 'Goa',
-  //     price: '7635',
-  //     tag: '/',
-  //   },
-  //   {
-  //     per: 'Ulimated',
-  //     off: 35,
-  //     duration: 4,
-  //     mode: 'Van',
-  //     imgSrc: img,
-  //     location: 'Ladakh',
-  //     price: '5899',
-  //     tag: '/',
-  //   },
-  //   {
-  //     per: 'Ulimated',
-  //     off: 20,
-  //     duration: 5,
-  //     mode: 'Train',
-  //     imgSrc: img,
-  //     location: 'Coorg',
-  //     price: '9999',
-  //     tag: '/',
-  //   },
-  // ];
 
 return (
     <>
@@ -89,7 +39,6 @@ return (
           <div className="homeCard grid">
             <div className="locationDiv">
               <label htmlFor="location">Source</label>
-              {/* <input id="src" type="text" placeholder="Source" required /> */}
               <div class="select">
                 <select id="src" name='location'>
                   <option value="">Source</option>
@@ -116,7 +65,6 @@ return (
 
             <div className="distDiv">
               <label htmlFor="distance">Destination</label>
-              {/* <input id="des" type="text" placeholder="Dream Destination" required /> */}
               <div class="select">
                 <select id="des" name='distance'>
                   <option value="">Destination</option>
@@ -163,7 +111,6 @@ return (
             </div>
             <div className="priceDiv">
               <label htmlFor="days">Days</label>
-              {/* <input id="days" type="number" placeholder="1-5" min={1} max={5} required /> */}
               <div class="select">
                 <select id="days" name='days'>
                   <option value="">Select Days</option>
@@ -187,59 +134,9 @@ return (
 <section className="offer container section">
       <div className="secContainer">
         <div id='heading' className="secIntro ">
-          {/* <h2 className="secTitle">Packages</h2> */}
         </div>
 
         <div id='offerlist' className="mainContent grid ">
-          {/* {Offer.map(({ off, duration, mode, imgSrc, per, location, price }) => {
-            const originalPrice = parseInt(price);
-            const discountedPrice = originalPrice - (originalPrice * off) / 100;
-
-            return (
-              <div className="singleOffer">
-                <div className="destImage">
-                  <img src={imgSrc} alt="{destTitle}" />
-                  <span className="discount">{off}% Off</span>
-                </div>
-
-                <div className="offerBody">
-                  <div className="price flex">
-                    <h4>
-                       ₹ {discountedPrice} <del className="canceled-price"> ₹ {originalPrice}</del>
-                    </h4>
-                    <span className="status">Special</span>
-                  </div>
-
-                  <div className="amenities flex">
-                    <div className="singleAmenity flex">
-                      <BsPersonFill className="icon" />
-                      <small>{per}</small>
-                    </div>
-                    <div className="singleAmenity flex">
-                      <GiDuration className="icon" />
-                      <small>{duration} days</small>
-                    </div>
-                    <div className="singleAmenity flex">
-                      <MdModeOfTravel className="icon" />
-                      <small>{mode}</small>
-                    </div>
-                  </div>
-
-                  <div className="location flex">
-                    <MdLocationOn className="icon" />
-                    <small>{location}</small>
-                  </div>
-
-                  <Link to="/booking">
-                    <button className="btn flex">
-                      Book
-                      <BsArrowRightShort className="icon" />
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </section>

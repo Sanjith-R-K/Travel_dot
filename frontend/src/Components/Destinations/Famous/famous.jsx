6006148ce21f5da5c5564573bb99d80839d6e97a
Sanjith_R_K <130780080+Sanjith-R-K@users.noreply.github.com>
2023-07-16 11:34:01 +0530

    Update famous.jsx

--- frontend/src/Components/Destinations/Famous/famous.jsx
+++ frontend/src/Components/Destinations/Famous/famous.jsx
@@ -109,10 +109,6 @@ const famous = () => {
           <p data-aos="fade-up" data-aos-duration="2500" className="subTitle">
             Travel to your favourite city with respectful of the environment!
           </p>
-{/* 
-          <button data-aos="fade-up" data-aos-duration="3000" className="btn">
-            <a href="#">Explore Now</a>
-          </button> */}
         </div>
       </div>
     </section>
